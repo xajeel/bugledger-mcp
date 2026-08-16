d85545b741bb140492078c7c7c8cf47179e74ebc
xajeel <sajeel135@gmail.com>
2026-08-16 18:54:17 +0500

    feat: mcp server entrypoint

--- python/main.py
+++ python/main.py
@@ -1,6 +1,6 @@
-def main():
-    print("Hello from python!")
+from src.bugledger_mcp.mcp_server import mcp
+from src.bugledger_mcp.tool_registery import *
 
 
 if __name__ == "__main__":
-    main()
+    mcp.run()
