801189ce6d42fbd09e71e1e5039b187397ec2bf4
xajeel <sajeel135@gmail.com>
2026-08-26 14:59:12 +0500

    feat: add http-not-https Semgrep rule

---
+++ shared/rules/http-not-https.py
@@ -0,0 +1,14 @@
+# ruleid: bugledger-http-not-https
+API_URL = "http://api.production.com/v1"
+
+# ruleid: bugledger-http-not-https
+WEBHOOK = "http://hooks.stripe.com/callback"
+
+# ok: bugledger-http-not-https
+API_URL = "https://api.production.com/v1"
+
+# ok: bugledger-http-not-https
+LOCAL = "http://localhost:8080"
+
+# ok: bugledger-http-not-https
+DEV = "http://127.0.0.1:3000"
